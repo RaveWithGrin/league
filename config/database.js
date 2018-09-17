19f083d86ffd254624312e81fddb5ceddd10e347
RaveWithGrin <ryanseanmillward@gmail.com>
2018-09-16 21:51:19 -0400

    Removing original config files

--- config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    connection: {
-        host: '192.168.2.58',
-        user: 'ryan',
-        password: 'Pirates91!db',
-        database: 'league',
-        connectionLimit: 100
-    }
-};
--- config/token.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    token: 'RGAPI-88c290c0-9270-4cc3-b527-38f62b2722a4'
-};
