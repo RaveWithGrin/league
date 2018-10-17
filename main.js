d7c293eaeb1ab04987e390e54aba75a9d4ba75b9
Ryan Millward <ryanseanmillward@gmail.com>
2018-10-17 20:08:56 +0000

    Addition of parser to main

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 var winston = require('winston');
 var Promise = require('bluebird');
-
-var myFormat = winston.format.printf(function(info) {
-    return info.timestamp + ' ' + info.level.toUpperCase() + ' ' + info.message;
-});
+var argv = require('yargs');
 
 var winstonLogger = winston.createLogger({
-    format: winston.format.combine(winston.format.timestamp(), myFormat),
+    format: winston.format.combine(winston.format.timestamp(), winston.format.printf(function(info){
+        return info.timestamp + ' ' + info.level.toUpperCase() + ' ' + info.message;
+    })),
     transports: [new winston.transports.Console()],
     level: 'info'
 });
@@ -19,24 +18,23 @@ var staticData = require('./staticData')(logger, api, db);
 var userData = require('./userData')(logger, api, db);
 var matchData = require('./matchData')(logger, api, db);
 var userTest = require('./singleUserTest')(logger, userData);
-
 var webserver = require('./webserver')(logger, staticData, userData, matchData, db);
-webserver.start();
 
-var getAllStaticData = async function() {
-    var staticDataPromises = [];
-    var champions = await staticData.getChampions();
-    staticDataPromises.push(staticData.saveChampions(champions.data));
-    var items = await staticData.getItems();
-    staticDataPromises.push(staticData.saveItems(items.data));
-    staticDataPromises.push(staticData.getRunes());
-    staticDataPromises.push(staticData.getSpells());
-    staticDataPromises.push(staticData.getSkins());
-    await Promise.all(staticDataPromises);
-    logger.info('Done getting static data');
-};
+argv.command('server [port]', 'Starts the webserver', {}, function(argv){
+    logger.info('Starting the webserver on port ' + (argv.port || 'default'));
+}).command('static', 'Gets static data', {}, function(argv){
+    logger.info('Getting static data');
+}).command('user <username>', 'Gets summoner data', {}, function(argv){
+    logger.info('Getting summoner data for ' + argv.username);
+}).demandCommand().command('matchlist [limit]', 'Gets data for matches in the DB', {}, function(argv){
+    logger.info('Getting data for ' + (argv.limit || 1) + ' matches at a time');
+}).command('newmatches [limit]', 'Gets new matches from new summoners', {}, function(argv){
+    logger.info('Getting matches for' + (argv.limit || 1) + 'summoners at a time');
+}).help().argv;
 
-//getAllStaticData();
+//webserver.start();
+
+//staticData.getAll();
 
 //userTest.pipeline('Rave With Grin');
 
